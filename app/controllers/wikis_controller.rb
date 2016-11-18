b870b9d6754910e642c70522896b4b98d52d2e31
Charlie Gaines <beaugaines@gmail.com>
2016-11-18 10:13:57 -0500

    move auth_user callback into application controller

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,7 +1,6 @@
 class WikisController < ApplicationController
-  
-  before_action :authenticate_user!
-  
+
+
   def index
     @wikis = policy_scope(Wiki)
   end
@@ -13,14 +12,14 @@ class WikisController < ApplicationController
   def new
     @wiki = Wiki.new
   end
-  
+
   def create
     @wiki = Wiki.new
     @wiki.title = params[:wiki][:title]
     @wiki.body = params[:wiki][:body]
     @wiki.private = params[:wiki][:private]
     @wiki.user = current_user
-    
+
     if @wiki.save
       flash[:notice] = "You created a new wiki."
       redirect_to wiki_path(@wiki)
@@ -33,13 +32,13 @@ class WikisController < ApplicationController
   def edit
     @wiki = Wiki.find(params[:id])
   end
-  
+
   def update
     @wiki = Wiki.find(params[:id])
     @wiki.title = params[:wiki][:title]
     @wiki.body = params[:wiki][:body]
     @wiki.private = params[:wiki][:private]
-    
+
     if @wiki.save
       flash[:notice] = "#{@wiki.title} is updated."
       redirect_to wiki_path(@wiki)
@@ -48,10 +47,10 @@ class WikisController < ApplicationController
       render :show
     end
   end
-  
+
   def destroy
     @wiki = Wiki.find(params[:id])
-    
+
     if @wiki.destroy
       flash[:notice] = "#{@wiki.title} is deleted."
       redirect_to action: :index
