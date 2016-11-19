0b830a39deefdb4e8487e61ebbcb11360336caec
Charlie Gaines <beaugaines@gmail.com>
2016-11-19 10:43:01 -0500

    clean up charges controller create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  # rescue_from Pundit::NotAuthorizedError, with: :unauthorized
+  rescue_from Pundit::NotAuthorizedError, with: :unauthorized
 
   include Pundit
   protect_from_forgery
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,11 +1,7 @@
 class Wiki < ActiveRecord::Base
-  
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
   belongs_to :user
   has_many :collaborators
   has_many :users, through: :collaborators
-  
-  
 end
