f41a7e9ac1789edfa0e9da4d4cfc2003165a1fa2
Yen <yenchieh86@hotmail.com>
2016-12-29 08:16:20 +0000

    need to fix for JS-snippets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,6 +41,18 @@ $(document).ready(function(){
         convertHtml(this, '#wiki-preview');
     });
     
+    var blocmetrics = {};
+        
+    blocmetrics.report = function(eventName) {
+        var event = {event: { name: eventName }};
+        var request = new XMLHttpRequest();
+        request.open("POST", "https://bloc-final-project-yenchieh86.c9users.io/api/events", true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(event));
+    };
+    
+    blocmetrics.report();
+    
 });
 
 
